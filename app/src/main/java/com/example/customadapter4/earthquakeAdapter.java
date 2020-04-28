9cb176dec8ae690e2a830b23b43f4f4ca80799ae
orrpeles <orr.backup37@gmail.com>
2020-04-28 13:38:53 +0300

    added string manipulation to location of earthquake

--- app/src/main/java/com/example/customadapter4/earthquakeAdapter.java
+++ app/src/main/java/com/example/customadapter4/earthquakeAdapter.java
@@ -37,9 +37,30 @@ public class earthquakeAdapter extends ArrayAdapter <quake> {
         TextView magnitude = convertView.findViewById(R.id.magnitude);
         magnitude.setText(qke.getMagnitude());
 
-        TextView location = convertView.findViewById(R.id.location);
-        location.setText(qke.getLocation());
+        // store location to string for manipulation
+        String locationFull = qke.getLocation();
+        // declare string separator
+        String locationSeparator = "of";
+        // declare the two strings
+        String offset;
+        String primary;
+        // if the string does not include 'of' we do not split string but use 'near the' in strings.xml
+        if (locationFull.contains(locationSeparator)) {
+            String[] parts = locationFull.split("of");
+            offset = parts[0] + locationSeparator;
+            primary = parts[1];
+        }
+        else {
+            offset = getContext().getString(R.string.near_the);
+            primary = locationFull;
+        }
+        // display in text views:
 
+        TextView Offset = convertView.findViewById(R.id.locationOffset);
+        Offset.setText(offset);
+
+        TextView Primary = convertView.findViewById(R.id.locationPrimary);
+        Primary.setText(primary);
 
         // create a new data object from time in milliseconds of the quake
         Date dateObject = new Date(qke.getTimeInMilliseconds());
@@ -52,7 +73,6 @@ public class earthquakeAdapter extends ArrayAdapter <quake> {
         TextView timeView = convertView.findViewById(R.id.time);
         String formattedTime = formatTime(dateObject);
         timeView.setText(formattedTime);
-
 
         return convertView;
 
